05927ba58df6c5030bc47e188f7baee80027e9b6
timon2313 <tima2313@gmail.com>
2025-01-27 02:47:06 +0300

    lint test

--- backend/cats/models.py
+++ backend/cats/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-
 User = get_user_model()
 
 
@@ -16,16 +15,9 @@ class Cat(models.Model):
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
-    owner = models.ForeignKey(
-        User, related_name='cats', 
-        on_delete=models.CASCADE
-        )
-    achievements = models.ManyToManyField(Achievement, through='AchievementCat')
-    image = models.ImageField(
-        upload_to='cats/images/', 
-        null=True,  
-        default=None
-        )
+    owner = models.ForeignKey(User, related_name="cats", on_delete=models.CASCADE)
+    achievements = models.ManyToManyField(Achievement, through="AchievementCat")
+    image = models.ImageField(upload_to="cats/images/", null=True, default=None)
 
     def __str__(self):
         return self.name
@@ -36,4 +28,4 @@ class AchievementCat(models.Model):
     cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.achievement} {self.cat}'
+        return f"{self.achievement} {self.cat}"
